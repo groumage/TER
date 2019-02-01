5db89f19dd05dfb2e7da1fc66379fe8420da33ea
guillaume <guillaume.roumage@gmail.com>
2019-02-01 17:35:15 +0100

    source code

---
+++ src/particule_1D.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+from numpy import *
+
+pos_ini = 0
+vit_ini = 5
+
+def particule_anime(dt, N):
+	length = (int)(math.floor(N/dt))
+	pos_old = pos_ini
+	vit_old = vit_ini
+	x = zeros(length)
+	return 10
+
+def draw():
+	plt.plot([f(x) for x in range(11)])
+	plt.show();
+
+draw()
