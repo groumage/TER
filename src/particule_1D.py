105bece88a286ce3cfe148294d5e8fdb2b5e3ab7
guillaume <guillaume.roumage@gmail.com>
2019-02-02 19:27:53 +0100

    ajout list

--- src/particule_1D.py
+++ src/particule_1D.py
@@ -2,20 +2,21 @@ from matplotlib import pyplot as plt
 from numpy import *
 
 def particule_anime(dt, N, L, nb_particules):
-	pos_ini = zeros(nb_particules)
+	#pos_ini = zeros(nb_particules)
+	pos_ini = []
 	for i in range(nb_particules):
-		pos_ini[i] = random.randint(0,L)
+		#pos_ini[i] = random.randint(0,L)
+		pos_ini.append(random.randint(0,L))
 
-	vit_ini = zeros(nb_particules)
+	#vit_ini = zeros(nb_particules)
+	vit_ini = []
 	for i in range(nb_particules):
-		vit_ini[i] = random.uniform(0.1,1)
+		#vit_ini[i] = random.uniform(0.1,1)
+		vit_ini.append(random.uniform(0.1,1))
 
 	length = (int)(math.floor(N/dt))
 	pos_old = pos_ini
 	vit_old = vit_ini
-	
-	x = zeros((length, nb_particules))
-	x[0] = array([pos_ini[0] for _ in range(nb_particules)])
     
 	fig = plt.figure('particule_1D')
 	ax = fig.add_subplot(111)
@@ -33,12 +34,11 @@ def particule_anime(dt, N, L, nb_particules):
         
 	for n in range(1, length):
 		pos_next = (pos_old + dt * vit_old) % L
-		x[n] = array([pos_next[i] for i in range(nb_particules)])
-		pos_old = pos_next
-        
 		for pt,i in zip(pts,range(nb_particules)):
-			pt.set_data(x[n,i],0)
-            
+			#pos_old[i] = (pos_old[i] + dt * vit_old[i]) % L
+			pt.set_data(pos_old[i],0)
+		#pos_old = pos_next
+
 		plt.pause(0.001)
 
 particule_anime(1,1000,100,10)
