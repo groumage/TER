9f0ab931911795644974172c3f7590759946d8ef
guillaume <guillaume.roumage@gmail.com>
2019-02-17 16:58:04 +0100

    maj

--- src/code_rapport/6_creation_densite.py
+++ src/code_rapport/6_creation_densite.py
@@ -3,13 +3,60 @@ import random as random
 import numpy as np
 import time as time
 
-N = 100
-nbr_pts = 10
+N = 10
+nbr_pts = 100
 
 fig = plt.figure('Densité')
 fig.suptitle("Détermination d'une densité à partir d'un nuage de points")
 ax = fig.add_subplot(121)
 
+pts = []
+for i in range(nbr_pts):
+	alpha = random.uniform(0,1)
+	c = float((1*1)+1+1/6)
+	beta = random.uniform(0,1) * c
+	if (beta <= float((alpha**2)+alpha+1/6)):
+		pts.append([alpha,beta])
+X_densite = np.linspace(0,1,N)
+Y_densite = [float((x**2)+x+1/6) for x in X_densite]
+plt.plot(X_densite,Y_densite)
+X_pts = [pts[x][0] for x in range(len(pts))]
+Y_pts = [pts[x][1] for x in range(len(pts))]
+plt.plot(X_pts, Y_pts, 'x')
+
+pts.sort()
+
+ax = fig.add_subplot(122)
+
+start_list = 0
+l = len(pts)
+x = np.linspace(0,1,N)
+y = np.zeros(N)
+tmp1 = time.clock()
+for i in range(N-1):
+	nbr_particules = 0
+	for j in range(start_list,l):
+		if (i/N <= pts[j][0] <= (i+1)/N):
+			nbr_particules += 1
+		else:
+			y[i] = nbr_particules
+			start_list = j
+			break
+
+nbr_particules = 0
+for i in range(start_list,l):
+	if(((N+(N-1))/2)/N <= pts[i][0] <= 1):
+		nbr_particules += 1
+y[N-1] = 2 * nbr_particules
+
+tmp2 = time.clock()
+
+print("Temps : " + str(tmp2-tmp1) + " sec")
+
+plt.plot(x,y,'-')
+plt.show()
+
+'''
 x = []
 y = []
 for i in range(nbr_pts):
@@ -29,6 +76,8 @@ l = len(x)
 x_densite = np.linspace(0,1,N)
 y_densite = np.zeros(N)
 x.sort()
+'''
+
 '''
 tmp1 = time.clock()
 for i in range(N-1):
